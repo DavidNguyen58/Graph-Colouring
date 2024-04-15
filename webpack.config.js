a5536acd3088d8affe5008345499359271eabda6
DavidNguyen58 <138356579+DavidNguyen58@users.noreply.github.com>
2024-04-15 17:35:21 +0200

    Added new files

--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,22 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
   mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ]
+  },
 };
