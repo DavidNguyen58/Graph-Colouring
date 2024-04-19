7007e04f77c721b893a0e765da2924b1e05412f7
sglre6355 <sglre6355@gmail.com>
2024-04-19 22:47:43 +0900

    Allow API access from any origin

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,12 +1,21 @@
 mod graph;
 
-use axum::{routing::post, Router};
+use axum::{
+    http::{header::CONTENT_TYPE, Method}, routing::post, Router
+};
 use graph::handle_request;
+use tower_http::cors::{Any, CorsLayer};
 
 #[tokio::main]
 async fn main() {
+    let cors = CorsLayer::new()
+        .allow_methods([Method::POST])
+        .allow_origin(Any)
+        .allow_headers([CONTENT_TYPE]);
+
     let app = Router::new()
-        .route("/color_graph", post(handle_request));
+        .route("/color_graph", post(handle_request))
+        .layer(cors);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8008").await.unwrap();
     axum::serve(listener, app).await.unwrap();
